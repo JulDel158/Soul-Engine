4e83584ced0d8de2e23121a4244f4f78d5c9c05e
Julio Delgado Abreu <69531890+JulDel158@users.noreply.github.com>
2026-02-13 13:44:32 -0400

    updating .gitignore

--- Source/cpp/main.cpp
+++ Source/cpp/main.cpp
@@ -5,6 +5,7 @@
 #include <cstdio>
 
 // math library includes
+#include "glm/glm.hpp"
 #include "glm/vec2.hpp"
 #include "glm/vec3.hpp"
 #include "glm/mat4x4.hpp"
@@ -19,26 +20,24 @@ enum class screen_size : uint16_t
 
 namespace
 {
-    auto error_callback(int error, const char* description) -> void
+    auto errorCallback(int error, const char* description) -> void
     {
         static_cast<void>(fprintf(stderr, "Error: %s\n", description));
     }
     
-    void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+    void inputCallback(GLFWwindow* window, int key, int scancode, int action, int mods) // NOLINT Required function signature
     {
         if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         {
             glfwSetWindowShouldClose(window, GLFW_TRUE);
         }
     }
-    static int buffer_width, buffer_height = 0;
-    void buffer_size_callback(GLFWwindow* window, int width, int height)
+    //static int buffer_width, buffer_height = 0;
+    void bufferSizeCallback(GLFWwindow* window, int width, int height) // NOLINT required function signature
     {
-        buffer_width = width;
-        buffer_height = height;
         if (window != nullptr)
         {
-            glViewport(0, 0, buffer_width, buffer_height);
+            glViewport(0, 0, width, height);
         }
     }
 }
@@ -49,37 +48,41 @@ struct vertex
     glm::vec3 col;
 };
  
-static const vertex vertices[3] =
+static constexpr vertex vertices[3] =
 {
     { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
     { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
     { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
 };
+
+// shaders
+namespace
+{
+    const char* VertexShaderText =
+   "#version 330\n"
+   "uniform mat4 MVP;\n"
+   "in vec3 vCol;\n"
+   "in vec2 vPos;\n"
+   "out vec3 color;\n"
+   "void main()\n"
+   "{\n"
+   "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+   "    color = vCol;\n"
+   "}\n";
  
-static const char* vertex_shader_text =
-"#version 330\n"
-"uniform mat4 MVP;\n"
-"in vec3 vCol;\n"
-"in vec2 vPos;\n"
-"out vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
- 
-static const char* fragment_shader_text =
-"#version 330\n"
-"in vec3 color;\n"
-"out vec4 fragment;\n"
-"void main()\n"
-"{\n"
-"    fragment = vec4(color, 1.0);\n"
-"}\n";
+    const char* FragmentShaderText =
+    "#version 330\n"
+    "in vec3 color;\n"
+    "out vec4 fragment;\n"
+    "void main()\n"
+    "{\n"
+    "    fragment = vec4(color, 1.0);\n"
+    "}\n";
+}
 
 int main(void)
 {
-    glfwSetErrorCallback(error_callback);
+    glfwSetErrorCallback(errorCallback);
     
     // Initialize the library
     if (!glfwInit())
@@ -88,7 +91,7 @@ int main(void)
     }
 
     // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(static_cast<int>(screen_size::width), static_cast<int>(screen_size::height), "Soul Engine", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(static_cast<int>(screen_size::width), static_cast<int>(screen_size::height), "Soul Engine", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
@@ -96,7 +99,7 @@ int main(void)
     }
 
     // Set input callback
-    glfwSetKeyCallback(window, key_callback);
+    glfwSetKeyCallback(window, inputCallback);
     
     // Make the window's context current
     glfwMakeContextCurrent(window);
@@ -115,43 +118,49 @@ int main(void)
     // Successfully loaded OpenGL
     printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
     
-    glfwSetFramebufferSizeCallback(window, buffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, bufferSizeCallback);
     
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     
-    GLuint vertex_buffer;
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
  
-    const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
+    const GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &VertexShaderText, nullptr);
+    glCompileShader(vertexShader);
  
-    const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
+    const GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &FragmentShaderText, nullptr);
+    glCompileShader(fragmentShader);
  
     const GLuint program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
     glLinkProgram(program);
+    glDetachShader(program, vertexShader);
+    glDetachShader(program, fragmentShader);
  
-    const GLint mvp_location = glGetUniformLocation(program, "MVP");
-    const GLint pos_location = glGetAttribLocation(program, "vPos");
-    const GLint col_location = glGetAttribLocation(program, "vCol");
+    const GLint mvpLocation = glGetUniformLocation(program, "MVP");
+    const GLint posLocation = glGetAttribLocation(program, "vPos");
+    const GLint colLocation = glGetAttribLocation(program, "vCol");
  
-    GLuint vertex_array;
-    glGenVertexArrays(1, &vertex_array);
-    glBindVertexArray(vertex_array);
-    glEnableVertexAttribArray(pos_location);
-    glVertexAttribPointer(pos_location, 2, GL_FLOAT, GL_FALSE,
+    GLuint vertexArray;
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+    glEnableVertexAttribArray(posLocation);
+    glVertexAttribPointer(posLocation, 2, GL_FLOAT, GL_FALSE,
                           sizeof(vertex), reinterpret_cast<void*>(offsetof(vertex, pos)));  // NOLINT(performance-no-int-to-ptr)
-    glEnableVertexAttribArray(col_location);
-    glVertexAttribPointer(col_location, 3, GL_FLOAT, GL_FALSE,
+    glEnableVertexAttribArray(colLocation);
+    glVertexAttribPointer(colLocation, 3, GL_FLOAT, GL_FALSE,
                           sizeof(vertex), reinterpret_cast<void*>(offsetof(vertex, col)));  // NOLINT(performance-no-int-to-ptr)
+    
+    // glEnable(GL_CULL_FACE);
+    // glCullFace(GL_BACK);
+    // glFrontFace(GL_CW);
     
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
@@ -164,13 +173,13 @@ int main(void)
  
         glm::mat4 m, p, mvp;
         m = glm::mat4(1.0f);
-        m = glm::rotate(m, static_cast<float>(glfwGetTime()), glm::vec3(1.0f, 1.0f, 1.0f));
+        m = glm::rotate(m, static_cast<float>(glfwGetTime()), glm::vec3(0.0f, 1.0f, 0.0f));
         p = glm::ortho( -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         mvp =  p * m;
  
         glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, reinterpret_cast<const GLfloat*>(&mvp));
-        glBindVertexArray(vertex_array);
+        glUniformMatrix4fv(mvpLocation, 1, GL_FALSE, reinterpret_cast<const GLfloat*>(&mvp));
+        glBindVertexArray(vertexArray);
         glDrawArrays(GL_TRIANGLES, 0, 3);
  
         glfwSwapBuffers(window);
@@ -198,3 +207,87 @@ int main(void)
 //     glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
 //     return Projection * View * Model;
 // }
+
+// sample from https://nicolbolas.github.io/oldtut/Basics/Tut01%20Making%20Shaders.html
+// GLuint CreateShader(GLenum eShaderType, const std::string &strShaderFile)
+// {
+//     GLuint shader = glCreateShader(eShaderType);
+//     const char *strFileData = strShaderFile.c_str();
+//     glShaderSource(shader, 1, &strFileData, NULL);
+//     
+//     glCompileShader(shader);
+//     
+//     GLint status;
+//     glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+//     if (status == GL_FALSE)
+//     {
+//         GLint infoLogLength;
+//         glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+//         
+//         GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+//         glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+//         
+//         const char *strShaderType = NULL;
+//         switch(eShaderType)
+//         {
+//         case GL_VERTEX_SHADER: strShaderType = "vertex"; break;
+//         case GL_GEOMETRY_SHADER: strShaderType = "geometry"; break;
+//         case GL_FRAGMENT_SHADER: strShaderType = "fragment"; break;
+//         }
+//         
+//         fprintf(stderr, "Compile failure in %s shader:\n%s\n", strShaderType, strInfoLog);
+//         delete[] strInfoLog;
+//     }
+//
+//     return shader;
+// }
+
+// GLuint CreateProgram(const std::vector<GLuint> &shaderList)
+// {
+//     GLuint program = glCreateProgram();
+//     
+//     for(size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
+//         glAttachShader(program, shaderList[iLoop]);
+//     
+//     glLinkProgram(program);
+//     
+//     GLint status;
+//     glGetProgramiv (program, GL_LINK_STATUS, &status);
+//     if (status == GL_FALSE)
+//     {
+//         GLint infoLogLength;
+//         glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+//         
+//         GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+//         glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+//         fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+//         delete[] strInfoLog;
+//     }
+//     
+//     for(size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
+//         glDetachShader(program, shaderList[iLoop]);
+//
+//     return program;
+// }
+
+/*
+*Buffer Object Usage Hints. Every time we draw something, we are changing the buffer object's data. 
+*OpenGL has a way to tell it that you will be doing something like this, 
+*and it is the purpose of the last parameter of glBufferData. 
+*This tutorial changed the allocation of the buffer object slightly, replacing:
+*
+*===========================================================================================
+*glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+*===========================================================================================
+*with this:
+*===========================================================================================
+*glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STREAM_DRAW);
+*===========================================================================================
+*
+*GL_STATIC_DRAW tells OpenGL that you intend to only set the data in this buffer object once. 
+*GL_STREAM_DRAW tells OpenGL that you intend to set this data constantly, generally once per frame. 
+*These parameters do not mean anything with regard to the API; they are simply hints to the OpenGL implementation. 
+*Proper use of these hints can be crucial for getting good buffer object performance when making frequent changes. 
+ */
+
+// wtf is going on git
