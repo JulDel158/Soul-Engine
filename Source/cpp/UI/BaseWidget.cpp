82160a96252466088ba3329d573a21da6426bef6
Julio Delgado Abreu <69531890+JulDel158@users.noreply.github.com>
2026-03-06 16:22:31 -0400

    updating base widget

--- Source/cpp/UI/BaseWidget.cpp
+++ Source/cpp/UI/BaseWidget.cpp
@@ -2,9 +2,24 @@
 
 #include "Utils/Logger.hpp"
 
+namespace
+{
+	constexpr auto ZERO_VECTOR_2_F = glm::vec2(0.0f);
+	constexpr auto HUNDRED_VECTOR_2_F = glm::vec2(100.0f);
+}
+
 BaseWidget::BaseWidget() :
-parent_(nullptr),
-neighbors_{nullptr}
+	parent_(nullptr),
+	neighbors_{nullptr},
+	position_(ZERO_VECTOR_2_F),
+	size_(HUNDRED_VECTOR_2_F),
+	rotation_(0.0f),
+	layer_(0), 
+	is_active_(true),
+	is_visible_(true),
+	is_highlighted_(false),
+	can_update_(true),
+	can_refresh_(true)
 {
 }
 
@@ -18,7 +33,51 @@ BaseWidget::~BaseWidget()
 	}
 }
 
-void BaseWidget::AddParent(BaseWidget& parent)
+void BaseWidget::Start()
+{
+}
+
+void BaseWidget::Update(const float deltaTime)
+{
+	if (can_refresh_)
+	{
+		render_list_.clear();
+	}
+}
+
+void BaseWidget::End()
+{
+}
+
+void BaseWidget::OnMouseEnter()
+{
+	is_highlighted_ = true;
+}
+
+void BaseWidget::OnMouseLeave()
+{
+	is_highlighted_ = false;
+}
+
+void BaseWidget::OnFocused()
+{
+	is_highlighted_ = true;
+}
+
+void BaseWidget::OnUnfocused()
+{
+	is_highlighted_ = false;
+}
+
+void BaseWidget::OnClick()
+{
+}
+
+void BaseWidget::OnSelected()
+{
+}
+
+void BaseWidget::SetParent(BaseWidget& parent)
 {
 	parent_ = &parent;
 }
@@ -28,7 +87,7 @@ void BaseWidget::AddChild(BaseWidget& child)
 	int a = 1;
 	int* b = &a;
 	children_.push_back(&child);
-	child.AddParent(*this);
+	child.SetParent(*this);
 }
 
 void BaseWidget::AddNeighbor(BaseWidget& neighbor, EWidgetNeighbor direction)
--- Source/hpp/UI/BaseWidget.hpp
+++ Source/hpp/UI/BaseWidget.hpp
@@ -1,24 +1,86 @@
 #ifndef BASE_WIDGET_H
 #define BASE_WIDGET_H
 
+#include "glm/ext/vector_float2.hpp"
+#include "glm/ext/vector_float3.hpp"
+
 #include "EngineDataStructures.hpp"
+#include "Rendering/Texture2D.hpp"
 
 #include <vector>
+#include <tuple>
 
 class BaseWidget
 {
 protected:
+	using RenderData = std::tuple<Texture2D, glm::vec2, glm::vec2, float, glm::vec3>;
+	using RenderList = std::vector<RenderData>;
+	
 	BaseWidget* parent_;
 	std::vector<BaseWidget*> children_;
 	BaseWidget* neighbors_[static_cast<unsigned int>(EWidgetNeighbor::Count)];
+	
+	RenderList render_list_;
+	
+	glm::vec2 position_;
+	glm::vec2 size_;
+	glm::vec2 scale_;
+	float rotation_;
+	unsigned int layer_;
+	bool is_active_;
+	bool is_visible_;
+	bool is_highlighted_;
+	bool can_update_;
+	bool can_refresh_;
 	
 public:
 	BaseWidget();
 	virtual ~BaseWidget();
 	
-	void AddParent(BaseWidget& parent);
+	virtual void Start();
+	virtual void Update(const float deltaTime);
+	virtual void End();
+	
+	// Input events
+	virtual void OnMouseEnter(); // Event triggered when cursor hovers over widget
+	virtual void OnMouseLeave(); // Event triggered when cursors stops hovering over widget
+	virtual void OnFocused(); // Event triggered when this widget becomes focused  via keyboard or controller
+	virtual void OnUnfocused(); // Event triggered when this widget loses focus via controller or keyboard
+	virtual void OnClick(); // Event triggered when mouse is clicked while hovering over widget
+	virtual void OnSelected(); // Event triggered when widget is focused and selected via input (ex. pressing enter on keyboard or 'A'/'Start' on controller
+	
+	// Setters
+	void SetParent(BaseWidget& parent);
 	void AddChild(BaseWidget& child);
 	void AddNeighbor(BaseWidget& neighbor, EWidgetNeighbor direction);
+	
+	inline void SetPosition(const glm::vec2 position)	{ position_ = position; }
+	inline void SetSize(const glm::vec2 size)			{ size_ = size; }
+	inline void SetScale(const glm::vec2 scale)			{ scale_ = scale; }
+	inline void SetRotation(const float rotation)		{ rotation_ = rotation; }
+	inline void SetLayer(const unsigned int layer)		{ layer_ = layer; }
+	inline void SetActive(const bool active)			{ is_active_ = active; }
+	inline void SetVisible(const bool visible)			{ is_visible_ = visible; }
+	inline void SetCanUpdate(const bool update)			{ can_update_ = update; }
+	inline void SetRefresh(const bool refresh)			{ can_refresh_ = refresh; }
+	
+	// Getters
+	inline BaseWidget* GetParent() const							{ return parent_; }
+	inline std::vector<BaseWidget*> GetChildren() const				{ return children_; }
+	inline BaseWidget* GetNeighbor(EWidgetNeighbor direction) const { return neighbors_[static_cast<unsigned int>(direction)]; }
+	
+	inline const RenderList& GetRenderList() const					{ return render_list_; }
+	
+	inline glm::vec2	GetPosition() const 						{ return position_; }
+	inline glm::vec2	GetSize() const								{ return size_; }
+	inline glm::vec2	GetScale() const							{ return scale_; }
+	inline float		GetRotation() const 						{ return rotation_; }
+	inline unsigned int	GetLayer() const							{ return layer_; }
+	inline bool			IsActive() const							{ return is_active_; }
+	inline bool			IsVisible() const							{ return is_visible_; }
+	inline bool			IsHighlighted() const						{ return is_highlighted_; }
+	inline bool			CanUpdate() const							{ return can_update_; }
+	inline bool			CanRefresh() const							{ return can_refresh_; }
 };
 
 #endif
